b22f62e6e34f3ac9be87492f8dc90df7379a88cc
Moritz Hoffmann <moritz@hoffmail.me>
2019-09-17 21:02:38 +0200

    Add csv log & download

--- backend-src/server.js
+++ backend-src/server.js
@@ -1,9 +1,20 @@
 const KoaStaticServer = require('koa-static-server');
+const KoaMount = require('koa-mount');
+const KoaServeList = require('koa-serve-list');
 const config = require('config');
+const fs = require('fs');
+const timestamp = require('time-stamp');
 
 const { io, app, server } = require('./server_singleton');
 
 const plots = config.get('plots');
+
+// log file name
+const logfile = `${timestamp('DD.MM.YYYY_HH:mm:ss')}.csv`;
+
+const stream = fs.createWriteStream(`logs/${logfile}`, {flags: 'a+'});
+
+stream.write(`# Hopper logfile created at: ${timestamp('DD.MM.YYYY HH:mm:ss')} \n`);
 
 app.use(KoaStaticServer({
 	rootDir: 'build',
@@ -13,8 +24,11 @@ app.use(KoaStaticServer({
 io.on('connection', (socket) => {
 	console.log('client connected');
 
-	socket.on('action', (msg) => {
-		console.log(msg);
+	socket.on('getLog', () => {
+		socket.emit('log', {
+			name: logfile,
+			data: fs.readFileSync(`logs/${logfile}`).toString(),
+		});
 	});
 
 	socket.on('disconnect', () => {
@@ -36,7 +50,26 @@ process.stdin.on('data', (chunk) => {
 		return;
 	}
 
-	io.emit('data', obj);
+	const unixts = (new Date()).getTime();
+
+	let line = '';
+
+	line += `${unixts}, `;
+
+	for(const datum of obj){
+		line += `${datum}, `;
+	}
+
+	line = line.slice(0, -2);
+
+	line += '\n';
+
+	stream.write(line);
+
+	io.emit('data', {
+		data: obj,
+		timestamp: unixts,
+	});
 });
 
 console.log(plots);
--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ import {
 	Container,
 	Navbar,
 	NavbarBrand,
+	Button,
+	Modal,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	Nav,
+	NavItem,
+	NavLink,
 } from 'reactstrap';
+import { JsonEditor as Editor } from 'jsoneditor-react';
+import 'jsoneditor-react/es/editor.min.css';
+
+import DownloadFile from 'js-file-download';
+
+import ace from 'brace';
+import 'brace/mode/json';
+import 'brace/theme/github';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -20,6 +36,10 @@ class App extends Component {
 			ts: [],
 			plots: [],
 		};
+
+		this.toggleModal = this.toggleModal.bind(this);
+		this.saveConfig = this.saveConfig.bind(this);
+		this.getLog = this.getLog.bind(this);
 	}
 
 	componentDidMount() {
@@ -38,21 +58,53 @@ class App extends Component {
 			this.setState({
 				ts: ts,
 				plots: plots,
-			})
+				modal: false,
+			});
 		});
 
 		socket.on('data', (data) => {
-			for(const [i, datum] of data.entries()){
-				this.state.ts[i].append(new Date().getTime(), datum);
+			for(const [i, datum] of data.data.entries()){
+				this.state.ts[i].append(data.timestamp, datum);
 			}
 		});
+
+		socket.on('log', (data) => {
+			DownloadFile(data.data.toString(), data.name);
+		});
 	}
+
+	getLog(){
+		socket.emit('getLog');
+	}
+
+	toggleModal() {
+		this.setState(prevState => ({
+			modal: !prevState.modal
+		}));
+	}
+
+	saveConfig() {
+		console.log('save!');
+		console.log(this.state.plots);
+		this.toggleModal();
+	}
+
+	config = {
+		test: 'test',
+	};
 
 	render() {
 		return (
 			<div>
 				<Navbar color="light" light expand="md">
 					<NavbarBrand>Hopper Simpleplot</NavbarBrand>
+					<Nav className="ml-auto" navbar>
+						<NavItem right>
+							<NavLink style={{cursor: 'pointer'}} onClick={this.getLog}>
+								<b>Get Latest Log</b>
+							</NavLink>
+						</NavItem>
+					</Nav>
 				</Navbar>
 				<Container>
 					{this.state.ts.map((ts, index) => (
@@ -74,6 +126,22 @@ class App extends Component {
 							/>
 						</div>
 					))}
+					<Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+						<ModalHeader toggle={this.toggle}>Edit Config</ModalHeader>
+						<ModalBody>
+							<Editor
+								value={this.state.plots}
+								onChange={this.handleChange}
+								ace={ace}
+								theme="ace/theme/github"
+								// schema={yourSchema}
+							/>
+						</ModalBody>
+						<ModalFooter>
+							<Button color="primary" onClick={this.saveConfig}>Save</Button>{' '}
+							<Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+						</ModalFooter>
+					</Modal>
 				</Container>
 			</div>
 		);
